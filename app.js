4a28e3a36f312d0e64b2b9d68836c16a9c43b5db
MusharrafSiddiqui92 <musharrafzafar@gmail.com>
2025-04-14 15:51:33 +0500

    update

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ function weatherApp() {
     .then(function (response) {
       console.log(response);
       document.querySelector("#temp").innerHTML = `${response.data.main.temp} &degC`;
-      document.querySelector("#humidity").innerHTML = `Humidity ${response.data.main.humidity} %`;
-      let sunriseUnix = response.data.sys.sunrise; 
+      // document.querySelector("#humidity").innerHTML = `Humidity ${response.data.main.humidity} %`;
+      let sunriseUnix = response.data.sys.sunrise;
       let sunriseDate = new Date(sunriseUnix * 1000);
       let localSunriseTime = sunriseDate.toLocaleTimeString();
       let sunsetUnix = response.data.sys.sunset;
@@ -19,19 +19,44 @@ function weatherApp() {
       document.querySelector("#sunRise").innerHTML = `Sunrise: ${localSunriseTime}`;
       document.querySelector("#sunSet").innerHTML = `SunSet: ${localSunsetTime}`;
       document.querySelector("#countryName").innerHTML = `Country:${response.data.sys.country}`;
+      document.querySelector("#LocalClock").innerHTML = `Local Time:${response.data.timezone}`;
+      // Weather Condition and Wallpaper Handling
+      let WeatherCondition = response.data.weather[0].main.toLowerCase(); // convert to lowercase for easier comparison
+      document.querySelector("#condition").innerHTML = WeatherCondition;
+      document.body.classList.remove("rain", "clouds", "clear", "default")
+      if (WeatherCondition === "rain"){
+        document.body.classList.add("rain")
+      }
+        else if(WeatherCondition ==="clouds")
+          {
+            document.body.classList.add("clouds")
+        }
+  else if (WeatherCondition === "clear"){
+    document.body.classList.add("clear")
+  }
+       else{
+        document.body.classList.add("default")
+       }
     })
     .catch(function (error) {
       document.querySelector("#temp").innerHTML = "Invalid City";
       document.querySelector("#humidity").innerHTML = "Something Went Wrong";
       document.querySelector("#sunRise").innerHTML = "Something Went Wrong";
       document.querySelector("#sunSet").innerHTML = "Something Went Wrong";
-      document.querySelector("#sunSet").innerHTML = "Something Went Wrong";
+      document.querySelector("#LocalClock").innerHTML = "Something Went Wrong";
+      document.querySelector("#condition").innerHTML = "Something Went Wrong";
     });
 }
+function getLocalTime(timezoneOffsetInSeconds) {
+  let utcTime = moment.utc(); // get current UTC time
+  let localTime = utcTime.add(timezoneOffsetInSeconds, 'seconds'); // add offset
+  return localTime.format('LTS'); // format to local time string
+  console.log(getLocalTime)
+}
+
 function time() {
   let currTime = moment().format('LTS');
 document.getElementById("clock").textContent=currTime;
 }
 time()
 setInterval(time , 1000)
-
